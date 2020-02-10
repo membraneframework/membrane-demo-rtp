d85eea0711450897584e0e0f5c79aade1cd3444d
Marek Kaput <marek.kaput@outlook.com>
2020-02-10 16:51:23 +0100

    move version spec to @version attribute in mix.exs (#1)

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule UdpTest.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :udp_test,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
